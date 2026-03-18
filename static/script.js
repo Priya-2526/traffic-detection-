d1440e922b204675a1c1e9e5aed4085da59ad55e
Priya-2526 <priyadas26052006@gmail.com>
2026-03-18 19:22:55 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -15,22 +15,3 @@ function startLoading() {
 
     }, 1000)
 
-
-let timer = 0
-let interval
-
-function startLoading() {
-
-    document.getElementById("loader").style.display = "block"
-
-    timer = 0
-
-    interval = setInterval(() => {
-
-        timer++
-        document.getElementById("timer").innerText = timer
-
-    }, 1000)
-
-
-}
