3cc4641e2515f13b3b856d028fcd94d71fda36c8
Priya Das <priyadas26052006@gmail.com>
2026-03-18 17:20:07 +0530

    add yolov3 weights

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+import os
+import shutil
+from detect import detect_traffic
+import shutil
+
+shutil.rmtree("static/uploads", ignore_errors=True)
+os.makedirs("static/uploads/result", exist_ok=True)
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "static/uploads"
+RESULT_FOLDER = "static/uploads/result"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(RESULT_FOLDER, exist_ok=True)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    result = None
+    image_path = None
+
+    if request.method == "POST":
+
+        file = request.files["image"]
+
+        if file:
+
+            filepath = os.path.join(
+                app.config["UPLOAD_FOLDER"], file.filename
+            )
+
+            file.save(filepath)
+
+            result = detect_traffic(filepath)
+
+            image_path = result["output_image"]
+
+    return render_template(
+        "index.html",
+        result=result,
+        image_path=image_path
+    )
+
+
+if __name__ == "__main__":
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
