55b36f9112d71b26241f0c6cd15670a14a662d43
Priya-2526 <priyadas26052006@gmail.com>
2026-03-20 14:16:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ def index():
     return render_template("index.html", result=result)
 
 
+import os
+
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
