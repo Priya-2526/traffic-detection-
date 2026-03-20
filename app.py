865ac820121e77f8eafe4a9b033ab4c6b60a6231
Priya-2526 <priyadas26052006@gmail.com>
2026-03-20 13:58:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,15 @@ from detect import detect_traffic
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "static/uploads"
-RESULT_FOLDER = "static/uploads/result"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-os.makedirs(RESULT_FOLDER, exist_ok=True)
-
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 
     result = None
-    image_path = None
 
     if request.method == "POST":
 
@@ -25,27 +21,13 @@ def index():
 
         if file:
 
-            filename = file.filename
+            path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+            file.save(path)
 
-            filepath = os.path.join(
-                app.config["UPLOAD_FOLDER"],
-                filename
-            )
+            result = detect_traffic(path)
 
-            file.save(filepath)
-
-            result = detect_traffic(filepath)
-
-            if result:
-                image_path = result["output_image"]
-
-    return render_template(
-        "index.html",
-        result=result,
-        image_path=image_path
-    )
+    return render_template("index.html", result=result)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
